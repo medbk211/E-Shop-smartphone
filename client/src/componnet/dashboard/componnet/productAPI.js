fd72de16d747284fd62ec0d0d9f4ea04713d064e
mohamedbriki@gmail.com <mohamed briki>
2024-12-21 14:32:00 +0100

    badlna consomation api

--- client/src/componnet/dashboard/componnet/productAPI.js
+++ client/src/componnet/dashboard/componnet/productAPI.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import api from '../api/axiosConfig'
+
+import api from '../../../api/axiosConfig'
 
 
 // Ajouter un produit
--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import ProductDetails from '../componnet/ProductDetails';
-import products from '../data/products.json'; // Assurez-vous de centraliser vos produits dans un fichier comme `products.js`
+
 
 
 const ProductPage = ({ addToCart }) => {
   const { id } = useParams();
-  const product = products.find((item) => item.id === parseInt(id));
+  // const product = products.find((item) => item.id === parseInt(id));
 
-  if (!product) {
-    return <div className="p-4 text-center">Produit introuvable</div>;
-  }
+  // if (!product) {
+  //   return <div className="p-4 text-center">Produit introuvable</div>;
+  // }
 
-  return (
-    <div>
-      <ProductDetails product={product} handleAddToCart={() => addToCart(product)} />
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <ProductDetails product={product} handleAddToCart={() => addToCart(product)} />
+  //   </div>
+  // );
 };
 
 export default ProductPage;
